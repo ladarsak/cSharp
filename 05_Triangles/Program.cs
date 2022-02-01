c415ca83645d3ac51966bc7b7f5dcf25b088dad1
Shulga <ladarsak@gmail.com>
2022-02-01 18:04:28 +0300

    [fractal]

--- 05_Triangles/Program.cs
+++ 05_Triangles/Program.cs
@@ -7,3 +7,27 @@ Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc); 
 Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+
+while(count < 1000) {
+    int rand = new Random().Next(0, 3);
+    if(rand == 0){
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if(rand == 1){
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+
+    if(rand == 2){
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+Console.WriteLine("+");
+count++;
+}
