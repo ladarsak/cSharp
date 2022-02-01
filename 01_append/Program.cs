b79e8b7e506beca031eef6be85e66a86dcf0d1fe
Shulga <ladarsak@gmail.com>
2022-02-01 15:00:28 +0300

    [append finished]

--- 01_append/Program.cs
+++ 01_append/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("1st integer:");
+//int numberA = Console.Read();
+//Console.WriteLine("2d integer:");
+//int numberB = Console.Read();
+//Console.WriteLine(numberA);
+//Console.WriteLine (numberB);
+
+int numA = 3;
+int numB = 5;
+int sum = numA  + numB;
+Console.WriteLine(sum);
