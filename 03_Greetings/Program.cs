cf24796d6100ae1a9b8569ca3e5017fe4231cd0a
Shulga <ladarsak@gmail.com>
2022-02-01 15:50:56 +0300

    [IfElse]

---
+++ 03_Greetings/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What's Your Name?");
+string username = Console.ReadLine();
+//Console.WriteLine(username);
+if(username.ToLower() == "dima") {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+else {
+    Console.WriteLine("Hello, I Don't Know you");
+}
