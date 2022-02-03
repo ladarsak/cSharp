57484364962ea844d462de94ae5585e804ff7aff
Shulga <ladarsak@gmail.com>
2022-02-03 18:46:01 +0300

    [even]

---
+++ 15_even/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a number");
+string number = Console.ReadLine();
+int ParsedNumber = int.Parse(number);
+double n = (double)ParsedNumber;
+double x = n % 2;
+
+if(x == 0) {
+    Console.Write(n);
+    Console.Write(" is even");
+}
+else {
+    Console.Write(n);
+    Console.Write(" is not even");
+}
