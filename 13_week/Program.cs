80dcedfd466ad16512f8fd54d018608b2e7e4c32
Shulga <ladarsak@gmail.com>
2022-02-03 17:27:08 +0300

    week

---
+++ 13_week/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] week = {"Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Suturday", "Sunday",};
+Console.WriteLine("Please enter a number from 1 to 7");
+string DayNumber = Console.ReadLine();
+int ParsedDayNumber = int.Parse(DayNumber);
+//Console.WriteLine(ParsedDayNumber);
+
+int i = ParsedDayNumber - 1;
+//Console.WriteLine(i);
+Console.Write("The ");
+Console.Write(ParsedDayNumber);
+Console.Write(" th day is ");
+Console.Write(week[i]);
