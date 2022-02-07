d0b8ed137f6c4739fe87a341a422344df56979e7
Shulga <ladarsak@gmail.com>
2022-02-07 11:23:45 +0300

    [evens from 1 to N]

---
+++ 16_even of N/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter a number");
+string number = Console.ReadLine();
+int ParsedNumber = int.Parse(number);
+// Making an array from 1 to a given number
+int[] nums = new int[ParsedNumber];
+int i = 0;
+while (i < ParsedNumber) {
+    nums[i] = i + 1;
+    i++;
+}
+// Choosing all evens
+int count = 0;
+while(count < nums.Length) {
+    int x = nums[count] % 2;
+    if(x == 0) {
+        Console.WriteLine(nums[count]);
+    }
+    
+    count++;
+}
