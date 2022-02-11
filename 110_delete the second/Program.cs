dc9c9d30068ae81658e297aa3efdada1ace16f1e
Shulga <ladarsak@gmail.com>
2022-02-11 16:21:24 +0300

    [seminar2lection2]

---
+++ 110_delete the second/Program.cs
@@ -0,0 +1,7 @@
+﻿/*int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int result = (num % 10) + (num / (10 * 10)) * 10;
+Console.WriteLine(result);*/
+int num = 323;
+Console.WriteLine(num % 10);
+Console.WriteLine(num / 100);
---
+++ 111_weekend/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] week = {"Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Suturday", "Sunday",};
+Console.WriteLine("Please enter a number from 1 to 7");
+string DayNumber = Console.ReadLine();
+int ParsedDayNumber = int.Parse(DayNumber);
+//Console.WriteLine(ParsedDayNumber);
+
+int i = ParsedDayNumber - 1;
+if(i > 4){
+    Console.WriteLine($"{week[i]} is a day off");
+}
+else {
+    Console.WriteLine($"{week[i]} is not a day off");
+}
